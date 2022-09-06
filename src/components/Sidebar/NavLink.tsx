a21d79d21d40983eba61ad87593f46a4876ad948
cleisonmp <cleison.mp@gmail.com>
2022-09-06 20:30:53 -0300

    feat: check active link :sparkles:

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,6 +1,7 @@
 import NextLink from 'next/link'
 import { Icon, Link as ChakraLink, Text } from '@chakra-ui/react'
 import { IconType } from 'react-icons'
+import { useRouter } from 'next/router'
 
 export interface NavLinkProps {
   title: string
@@ -9,11 +10,18 @@ export interface NavLinkProps {
   // iconSize: string | number
 }
 export const NavLink = ({ title, href, icon }: NavLinkProps) => {
+  const router = useRouter()
+  const isActive = router.asPath.startsWith(href)
+
   return (
     <NextLink href={href} passHref>
-      <ChakraLink display="flex" alignItems="center">
+      <ChakraLink
+        display='flex'
+        alignItems='center'
+        color={isActive ? 'primaryPurple' : ''}
+      >
         <Icon as={icon} fontSize={20} />
-        <Text ml="4" fontWeight="medium">
+        <Text ml='4' fontWeight='medium'>
           {title}
         </Text>
       </ChakraLink>
