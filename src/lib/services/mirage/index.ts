f945d0d1596387b22616819de77867facdf0c4d3
cleisonmp <cleison.mp@gmail.com>
2022-09-29 17:53:36 -0300

    feat: add mirage pagination :sparkles:

--- src/lib/services/mirage/index.ts
+++ src/lib/services/mirage/index.ts
@@ -1,4 +1,4 @@
-import { createServer, Factory, Model } from 'miragejs'
+import { createServer, Factory, Model, Response } from 'miragejs'
 import { faker } from '@faker-js/faker'
 import { User } from '../../models/user'
 
@@ -26,13 +26,35 @@ export const makeServer = () => {
     },
 
     seeds(server) {
-      server.createList('user', 10)
+      server.createList('user', 174)
     },
 
     routes() {
       this.namespace = 'api'
       this.timing = 750
-      this.get('/users')
+      this.get('/users', function (schema, request) {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        const { page = 1, per_page = 10 } = request.queryParams
+
+        const allUsers = schema.all('user')
+        const totalUsers = allUsers.length
+
+        const pageStart = (Number(page) - 1) * Number(per_page)
+        const pageEnd = pageStart + Number(per_page)
+
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        const users = this.serialize(allUsers).users.slice(pageStart, pageEnd) // eslint-disable-line
+
+        return new Response(
+          200,
+          {
+            'x-total-count': String(totalUsers),
+          },
+          { users },
+        )
+      })
       this.post('/users')
       this.namespace = ''
       this.passthrough()
